08e686797ba009ce0e09604f3559bb79287a99b5
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2020-06-29 19:40:13 -0300

    Se agregaron notas aclaratorias

--- Ejercicios05Punto04Version01/Program.cs
+++ Ejercicios05Punto04Version01/Program.cs
@@ -6,13 +6,18 @@ namespace Ejercicios05Punto04Version01
     {
         static void Main(string[] args)
         {
-			var sumaAzar=0;
-            var contadorNros = 0;
-            int nroAzar;
+			var sumaAzar=0;//variable de tipo entero para acumular los nros generados
+            var contadorNros = 0;//variable de tipo entero para cotnar los nros generados
+            //Random es un objeto que genera nros al azar
+            //Para poderlo utilizar lo debo instanciar primero
+            //Debo crear un objeto de tipo random y asignarlo a una variable
             Random r=new Random();//Creación de una variable r de tipo Random (al azar)
             do
             {
-                nroAzar=r.Next(1, 101);//Genera el nro al azar entre 1 y 100
+                /*El método Next del objeto Random
+                 genera números al azar entre el menor y uno menos que el mayor
+                la sintaxis es siempre r.Next(nMenor, nMayor)*/
+                var nroAzar=r.Next(1, 101);//Genera el nro al azar entre 1 y 100
                 Console.WriteLine($"{nroAzar}");//Muestro el nro generado
                 sumaAzar += nroAzar;//lo acumulo
                 contadorNros++;//lo cuento
